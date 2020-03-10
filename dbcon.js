022c36371f74380b5670c36069f35b7eafd656d4
Jonah Dubbs-Nadeau <jonah.dubbs.nadeau@gmail.com>
2020-03-09 22:32:19 -0700

    Update dbcon back

--- dbcon.js
+++ dbcon.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql2/promise');
 
 var pool = mysql.createPool({
-    socketPath:      '/cloudsql/responsive-icon-270702:us-central1:oregon-family-medical',
+    host:            '35.223.94.139',
     user:            'root',
     password:        'rqB38qh9LCD7oaP4',
     database:        'cs340'
